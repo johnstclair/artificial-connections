fa20b2730943d328a5b5c0d2fb5ae257fa9cfa17
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-01-13 17:42:12 -0800

    ci: reworked the selection logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [blocks, setBlock] = useState(["veil","shake","cable","vision","arrange","offspring","fund","ridge","authorize","parade","suffering","impound","bad","concentration","slippery","artichoke"]);
-  const [selected, setSelected] = useState(new Array(16).fill(false));
+  const wordList = ["veil","shake","cable","vision","arrange","offspring","fund","ridge","authorize","parade","suffering","impound","bad","concentration","slippery","artichoke"];
+  const [selected, setSelected] = useState(new Array(4).fill(""));
+  const [blocks, setBlocks] = useState<(string | boolean)[][]>([]);
 
   console.log("selected " + selected);
 
-  function handleSelection(wordIndex) {
-    let temp = selected;
-    temp[wordIndex] =  !temp[wordIndex];
+  useEffect(() => {
+    let temp: (string | boolean)[][] = [];
+    wordList.map((word) => {
+      temp.push([word,false]);
+    });
+    setBlocks(temp);
+  }, []);
+
+  function handleSelection(index: number) {
+    let temp = blocks;
+    temp[index][1] =  !temp[index][1];
     setSelected(temp);
+
+    console.log(blocks)
   }
 
   function handleSubmit() {
-    let words = [];
+    let words: string[] = [];
 
     selection.map((index, selected) => {
       if (selected) {
         words.push(blocks[index])
       }
     });
-
-    console.log(words)
   }
 
   return (
   <>
       <div className="button-grid-container">
-        {blocks.map((word, index) => {
-          return <button onClick={(e) => handleSelection(index)} className="button-grid">{word.toUpperCase()}</button>
-          console.log({word});
+        {blocks.map((item, index) => {
+          return <button key={index} onClick={(e) => handleSelection(index)} className="button-grid">{item[0].toUpperCase()}</button>
         })}
       </div>
-      <button onClick{(e) => handleSubmit()}>SUBMIT</button>
+      <button >SUBMIT</button>
   </>
   );
 }
