fefed79880a755dd6ab6bbc6127343630da74c98
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-01-13 17:01:58 -0800

    ci: thigns

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,37 @@ import "./App.css";
 
 function App() {
   const [blocks, setBlock] = useState(["veil","shake","cable","vision","arrange","offspring","fund","ridge","authorize","parade","suffering","impound","bad","concentration","slippery","artichoke"]);
+  const [selected, setSelected] = useState(new Array(16).fill(false));
 
+  console.log("selected " + selected);
 
+  function handleSelection(wordIndex) {
+    let temp = selected;
+    temp[wordIndex] =  !temp[wordIndex];
+    setSelected(temp);
+  }
+
+  function handleSubmit() {
+    let words = [];
+
+    selection.map((index, selected) => {
+      if (selected) {
+        words.push(blocks[index])
+      }
+    });
+
+    console.log(words)
+  }
 
   return (
   <>
       <div className="button-grid-container">
-        {blocks.map((word) => {
-          return <button className="button-grid">{word}</button>
-          console.log({word})
+        {blocks.map((word, index) => {
+          return <button onClick={(e) => handleSelection(index)} className="button-grid">{word.toUpperCase()}</button>
+          console.log({word});
         })}
       </div>
+      <button onClick{(e) => handleSubmit()}>SUBMIT</button>
   </>
   );
 }
