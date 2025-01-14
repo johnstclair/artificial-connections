abda0b45ee3b39b7366a22cec6753c9e3ba03b56
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-01-13 17:19:45 -0800

    feat nix dev env

---
+++ shell.nix
@@ -0,0 +1,24 @@
+let
+  pkgs = import <nixpkgs> { };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    gobject-introspection
+  ];
+
+  buildInputs = with pkgs;[
+    at-spi2-atk
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gtk3
+    harfbuzz
+    librsvg
+    libsoup_3
+    pango
+    webkitgtk_4_1
+    openssl
+  ];
+}
