8e036e90e119e14945d5608129911e6eba4749ef
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-01-18 18:06:06 -0800

    feat: reacter router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import GameScreen from "./components/GameScreen.tsx"
+import GameScreen from "./components/GameScreen.tsx";
+import Start from "./components/Start.tsx";
 import "./App.css";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
 
 function App() {
-  return <>
-    <GameScreen/>
-  </>
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Start />} />
+                <Route path="/play" element={<GameScreen />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Start.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+function Start() {
+  return <>
+    <Link to="play">
+      <button>clock me</button>
+    </Link>
+  </>
+}
+export default Start;
